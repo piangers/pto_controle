a8f52b28842a3b95460332f0a3a2a62d5ca084a6
Piangers <condeczar@gmail.com>
2018-04-03 14:05:19 -0300

    boilerplate script

---
+++ extrai_dados/extrai_dados.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+Name                 : Extrai dados processamento PPP e TBC
+Description          : Extrai dados do relatório de processamento em PDF do PPP e TBC e realiza o controle de qualidade
+Version              : 0.1
+copyright            : 1ºCGEO / DSG
+reference:
+ ***************************************************************************/
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
+##DSG=group
+##data=string
+##medidores=string
+##pasta=folder
+##fuso_horario=number -3
+##log=output file
+
+if __name__ == '__builtin__':
+    pass
+
+
+if __name__ == '__main__':
+    pass
