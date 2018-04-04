bfaa1cf1474389d8cd7dfadfa9a134533d021ea8
Felipe Diniz <diniz.ime@gmail.com>
2018-04-04 17:26:47 -0300

    refactor

--- extrai_dados/extrai_dados.py
+++ extrai_dados/extrai_dados.py
@@ -17,8 +17,7 @@ reference:
  ***************************************************************************/
 """
 ##DSG=group
-##pasta_ppp=folder
-##pasta_tbc=folder
+##pasta_dados=folder
 ##csv_file=output file
 
 import PyPDF2
@@ -26,10 +25,17 @@ import os
 import os.path
 import math
 import sys
+from re import search
 
 def lerPPP(pasta):
     # caminho para os pdfs
-    pdfs = [os.path.join(pasta, nome) for nome in os.listdir(pasta) if nome.endswith('.pdf') and os.path.isfile(os.path.join(pasta, nome))]
+    pdfs = []
+    for root, dirs, files in os.walk(pasta):
+        if root.split('\\')[-1] == "6_Processamento_PPP" and len(files) > 0:
+            for f in files:
+                if f.endswith('.pdf') and os.path.isfile(os.path.join(root, f)):
+                    pdfs.append(os.path.join(root, f))
+
     conteudo_list = []
     for pdf in pdfs:
         # abre o arquivo pdf e cria um objeto reader
@@ -49,40 +55,52 @@ def lerPPP(pasta):
 
 def lerTBC(pasta):
     # caminho para os pdfs
-    pdfs = [os.path.join(pasta, nome) for nome in os.listdir(pasta) if nome.endswith('.pdf') and os.path.isfile(os.path.join(pasta, nome))]
+    pdfs = []
+    for root, dirs, files in os.walk(pasta):
+        if root.split('\\')[-1] == "7_Processamento_TBC_RBMC" and len(files) > 0:
+            for f in files:
+                if f.endswith('.pdf') and os.path.isfile(os.path.join(root, f)):
+                    pdfs.append(os.path.join(root, f))
+
     conteudo_list = []
     for pdf in pdfs:
-        # abre o arquivo pdf e cria um objeto reader
         with open(pdf, "rb") as f:
-            conteudo = PyPDF2.PdfFileReader(f)       # objeto que representa o documento
-            # Itera pelas páginas do documento
+            conteudo = PyPDF2.PdfFileReader(f)    
             tbc = {}
             text = conteudo.getPage(0).extractText()
-            tbc[u'nome_ponto'] = text.split('\n')[79]
-            tbc[u'TBC_E'] = text.split('\n')[84].replace(' m', '').replace(',', '.')
-            tbc[u'TBC_N'] = text.split('\n')[90].replace(' m', '').replace(',', '.')
-            tbc[u'TBC_h'] = text.split('\n')[96].replace(' m', '').replace(',', '.')
-            # fecha o arquivo
+            pto_regex = "^(RS|PR|SC|SP)-(HV|Base)-[1-9]+[0-9]*$"
+            x = [ i for i, pto in enumerate(text.split('\n')) if search(pto_regex, pto)][0]
+            tbc[u'nome_ponto'] = text.split('\n')[x]
+            tbc[u'TBC_E'] = text.split('\n')[x+5].replace(' m', '').replace(',', '.')
+            tbc[u'TBC_N'] = text.split('\n')[x+11].replace(' m', '').replace(',', '.')
+            tbc[u'TBC_h'] = text.split('\n')[x+17].replace(' m', '').replace(',', '.')
             conteudo_list.append(tbc)
     return conteudo_list
-    
+
+def merge_lists(l1, l2, key):
+    merged = {}
+    for item in l1+l2:
+        if item[key] in merged:
+            merged[item[key]].update(item)
+        else:
+            merged[item[key]] = item
+    return [val for (_, val) in merged.items()]
+
 def criaCSV(nome_csv, ppp_list, tbc_list):
-    for ppp in ppp_list:
-        for tbc in tbc_list:
-            if tbc["nome_ponto"] == ppp["nome_ponto"]:
-                ppp[u'TBC_E'] = tbc[u'TBC_E']
-                ppp[u'TBC_N'] = tbc[u'TBC_N']
-                ppp[u'TBC_h'] = tbc[u'TBC_h']
-        
-        ppp["Delta_E"] = round(float(ppp[u'PPP_E']) - float(ppp[u'TBC_E']),3)
-        ppp["Delta_N"] = round(float(ppp[u'PPP_N']) - float(ppp[u'TBC_N']),3)
-        ppp["Delta_h"] = round(float(ppp[u'PPP_h']) - float(ppp[u'TBC_h']),3)
-        ppp["Delta_EN"] = round(math.sqrt(ppp["Delta_E"]**2 + ppp["Delta_N"]**2),3)
-      
+    merged = merge_lists(ppp_list, tbc_list, 'nome_ponto')
+    erros = []
     with open(nome_csv, 'w') as f:
         f.write('nome_ponto,PPP_N,PPP_E,PPP_h,PPP_H,TBC_N,TBC_E,TBC_h,Delta_N,Delta_E,Delta_h,Delta_EN\n')
-        for ppp in ppp_list:
-            f.write('{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11}\n'.format(ppp["nome_ponto"], ppp["PPP_N"], ppp["PPP_E"], ppp["PPP_h"], ppp["PPP_H"], ppp["TBC_N"], ppp["TBC_E"], ppp["TBC_h"], ppp["Delta_N"], ppp["Delta_E"], ppp["Delta_h"], ppp["Delta_EN"]))
+        for pto in merged:
+            try:
+                pto["Delta_E"] = round(float(pto[u'PPP_E']) - float(pto[u'TBC_E']),3)
+                pto["Delta_N"] = round(float(pto[u'PPP_N']) - float(pto[u'TBC_N']),3)
+                pto["Delta_h"] = round(float(pto[u'PPP_h']) - float(pto[u'TBC_h']),3)
+                pto["Delta_EN"] = round(math.sqrt(pto["Delta_E"]**2 + pto["Delta_N"]**2),3)
+                f.write('{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11}\n'.format(pto["nome_ponto"], pto["PPP_N"], pto["PPP_E"], pto["PPP_h"], pto["PPP_H"], pto["TBC_N"], pto["TBC_E"], pto["TBC_h"], pto["Delta_N"], pto["Delta_E"], pto["Delta_h"], pto["Delta_EN"]))
+            except:
+                erros.append(pto["nome_ponto"])
+    return erros
 
 
 if __name__ == '__builtin__':
@@ -91,10 +109,10 @@ if __name__ == '__builtin__':
     criaCSV(csv_file, ppp,tbc)
 
 if __name__ == '__main__':
-    if len(sys.argv) >= 4:
+    if len(sys.argv) >= 3:
         tbc = lerTBC(sys.argv[1])
-        ppp = lerPPP(sys.argv[2])
-        criaCSV(sys.argv[3], ppp,tbc)
+        ppp = lerPPP(sys.argv[1])
+        print criaCSV(sys.argv[2], ppp,tbc)    
     else:
         print(u'Parametros incorretos!')
 
