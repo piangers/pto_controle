c75df5c90498af2c790338980da5f423191905ac
Felipe Diniz <diniz.ime@gmail.com>
2018-04-05 11:59:09 -0300

    small test

--- upload_ppp/upload_ppp.js
+++ upload_ppp/upload_ppp.js
@@ -4,22 +4,33 @@ fixture`Download all`;
 
 const page = `https://www.ibge.gov.br/geociencias-novoportal/informacoes-sobre-posicionamento-geodesico/servicos-para-posicionamento-geodesico/16334-servico-online-para-pos-processamento-de-dados-gnss-ibge-ppp.html?=&t=processar-os-dados`;
 
-test.page(page)("Download", async t => {
-  await t
-    .typeText(
-      Selector("input").withAttribute("name", "email"),
-      "diniz.ime@gmail.com"
-    )
-    .setFilesToUpload(
-      Selector("input").withAttribute("name", "arquivo"),
-      "C:\\Users\\Diniz\\Downloads\\alegranzi_2018-03-27-20180330T112916Z-001\\alegranzi_2018-03-27\\RS-HV-983\\2_RINEX\\RS-HV-983.zip"
-    )
-    .click(Selector("input").withAttribute("value", "Processar"))
-    .wait(20000)
-    .switchToIframe("#iframe_resultado")
-    .click(Selector("a"))
-    .wait(5000);
-});
-//npm install -g testcafe
+const paths = [
+  'E:\\RS-HV-1252.zip',
+  'E:\\RS-HV-1253.zip'
+]
 
-//testcafe chrome upload_ppp.js
+paths.forEach(path => {
+  test.page(page)("Download", async t => {
+    await t
+      .typeText(
+        Selector("input").withAttribute("name", "email"),
+        "diniz.ime@gmail.com"
+      )
+      .setFilesToUpload(
+        Selector("input").withAttribute("name", "arquivo"),
+        path
+      )
+      .click(Selector("input").withAttribute("value", "Processar"))
+      .wait(5000);
+      
+    await t
+      .switchToIframe("#iframe_resultado")
+      .wait(20000)
+      .click(Selector("a"))
+      .wait(5000);
+  });
+});
+
+
+//npm install -g testcafe
+//testcafe -c 2 chrome upload_ppp.js
