f1e9156b34fdff203ff7ae98bec3f3e7ec019f8a
Piangers <condeczar@gmail.com>
2018-04-09 17:26:00 -0300

    cria_pastas funcionando

--- upload_ppp/preparo_ppp.py
+++ upload_ppp/preparo_ppp.py
@@ -1,18 +1,4 @@
 # -*- coding: utf-8 -*-
-''' ___________________________________________________________________
-         
-         se_nao_existe_pasta_com_este_nome = not os.path.isdir(pasta1)
-         entao_cria = os.mkdir(pasta1)
-        if se_nao_existe_pasta_com_este_nome:
-        
-        else:
-             entao_cria 
-    ___________________________________________________________________'''
-
-
-##DSG=group
-##pasta=folder
-##zip_file=input file
 
 import zipfile
 import os
@@ -20,34 +6,20 @@ import os.path
 from re import search
 
 
-def cria_pastas(pasta):
-    # caminho para os .TXT apenas para referencia
-    caminho = '/home/piangers/Documentos/desenvolvimento/preparo/2018-04-04'
-    padrao  = "^(RS|PR|SC|SP)-(HV|Base)-[1-9]+[0-9]*$"
-    x = [ i for i, pto in enumerate(text.split('\n')) if search(padrao, pto)][0]
-    
-    for root, dirs in os.walk(pasta):
-        if root.split('\\')[-1] == nome_pasta:
-            
-            for di in dirs:
-                if not os.path.isdir('6_Processamento_PPP'):
-                    os.mkdir('6_Processamento_PPP') # cria pasta caso nao exista
-                    print ('Pasta criada com sucesso!')
+def cria_pastas(caminho):
+    pasta1 = '6_Processamento_PPP'
+    pasta2 = '7_processamento_TBC_RBMC'
+    padrao = "^(RS|PR|SC|SP)-(HV|Base)-[1-9]+[0-9]*$"
+
+    for root, dirs, files in os.walk(caminho):
+        for d in dirs:
+            print d, search(padrao,d)
+            if search(padrao,d):
+                a = os.path.join(caminho,d,pasta1)
+                os.mkdir(a)
+                b = os.path.join(caminho,d,pasta2)
+                os.mkdir(b)
                 
-                if not os.path.isdir('7_Processamento_TBC_RBMC'):
-                    os.mkdir('7_Processamento_TBC_RBMC') # cria pasta caso nao exista
-                    print ('Pasta criada com sucesso!')
+                
 
-        '''     if not os.path.exists(caminho):
-                    os.makedirs(caminho)
-                elif not os.path.isdir(caminho):
-                raise IOError(caminho + " nao eh um diretorio!")'''
-        
-
-
-
-
-if __name__ == '__builtin__':
-    cria= cria_pastas()
-    zipa= zipa_arquivos()
-   
+cria_pastas(R'/home/piangers/Documentos/desenvolvimento/preparo/2018-04-04')
---
+++ upload_ppp/teste.py
@@ -0,0 +1,14 @@
+import os.path
+import os
+
+
+
+def cria(caminho):
+
+   caminho.os.mkdir('6_Processamento_PPP')
+
+
+
+
+
+cria(R'/home/piangers')
