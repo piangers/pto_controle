a13ad18f9994b014fb61fe019e2844b676550b1c
Piangers <condeczar@gmail.com>
2018-04-09 18:02:26 -0300

    versao inicial zip

--- download_ppp/zipa.py
+++ download_ppp/zipa.py
@@ -1,20 +1,19 @@
-  
-def zipa_arquivos(arqs):
+# -*- coding: utf-8 -*-
+
+import zipfile
+import os
+import os.path
+from re import search
+
+def zipa_arquivos(pasta):
     nome_pasta = "^(RS|PR|SC|SP)-(HV|Base)-[1-9]+[0-9]*$"
     nome_zip = nome_pasta
     fls = []
     for root, dirs, files in os.walk(pasta):
-        if root.split('\\')[-1] == "2_RINEX" and len(files) > 0:
-            for fl in files:
-                if fl.endswith('.txt') and os.path.isfile(os.path.join(root, fl)):
-                    fls.append(os.path.join(root, fl))
-                    with zipfile.ZipFile(nome_pasta,'w', zipfile.ZIP_DEFLATED) as compactar:
-                        for arq in arqs:
-                            if(os.path.isfile(arq)): # se for ficheiro
-                                compactar.write(arq)
-                            else: # se for diretorio
-                                for root, dirs, files in os.walk(arq):
-                                    for pasta in files:
-                                        compactar.write(os.path.join(root, pasta), os.path.relpath(os.path.join(folder,file), 'C:\\'), compress_type = zipfile.ZIP_DEFLATED)
+        for f in files:
+            print f
+            
+             
 
-                                        print fls
+
+zipa_arquivos(R'/home/piangers/Documentos/desenvolvimento/preparo/2018-04-04')
