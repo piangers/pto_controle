3e516941db824cbd72fd832487d8e6b5d02c0243
Piangers <condeczar@gmail.com>
2018-04-11 14:34:27 -0300

    Zip versão concluída

--- download_ppp/zipa.py
+++ download_ppp/zipa.py
@@ -8,16 +8,18 @@ from re import search
 def zipa_arquivos(pasta):
     nome_arq = "^(RS|PR|SC|SP)-(HV|Base)-[1-9]+[0-9]\.+[0-9]+[0-9]+(o|n)*$" 
     
-    caminho=[]
+    
     for root, dirs, files in os.walk(pasta):
         if root.split('/')[-1] == "2_RINEX" and len(files)>0 :
-            with ZipFile("nome.zip", 'w') as novo_zip:
+            nome = root.split('/')[-2]
+            with ZipFile(root+'/'+nome+".zip", 'w') as novo_zip:
                 for fl in files:
                     if search(nome_arq,fl):
-                    #print search(nome_arq,fl)
-                        caminho.append(os.path(root,fl))
-                        novo_zip.write(os.path.join(caminho))
-                
+                        #print search(nome_arq,fl)
+                        os.path.join(root,fl)
+                        novo_zip.write(os.path.join(root,fl),fl)
+                        
+                        
                 
                 
                 
