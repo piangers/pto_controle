4322b6becf32b2881cd4912d04049993e938b50b
Piangers <condeczar@gmail.com>
2018-04-13 18:30:48 -0300

    modelo de apoio extrai_lista em desenvolvimento

--- download_ppp/extrai_dados_zip.py
+++ download_ppp/extrai_dados_zip.py
@@ -35,12 +35,23 @@ def busca_zip(local_zip,local_destino):
                 s2 = s1.split('.')[0] # s2 recebe o trecho do nome do ZIP selecionado, que será comparado com o do endereço
                 lista_zip.append(s2)
                 lista_zip.sort()
-                            
+        #print lista_zip[1]
+                listas = lista_zip, endereco                
                 # Preciso realizar comparações de listas para definir onde serão realizadas as extrações ZIP.
                 # Para cada comparação ele deve buscar o trecho do nome correspondente, no código endereço e comparar
                 # com o trecho do nome do zip para extrair na pasta correspondente, caso contrario não. 
                 
-                
+                pos_i = 0 # variável provisória de índice
+                pos_j = 0 # idem
+
+            for i in range (len(listas)): # procurar em todas as listas internas
+                for j in range (i): # procurar em todos os elementos nessa lista
+                    if s2 in listas[i][j]: # se encontrarmos elemento ('ana')
+                        pos_i = i # guardamos o índice i
+                        pos_j = j # e o índice j
+                    break # saímos do loop interno
+                break # e do externo
+            return (pos_i, pos_j) # e retornamos os índices
             
                     #zip = ZipFile(os.path.join(root, f))  
                     #zip.extract(endereco) 
