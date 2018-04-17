3e4dff069f516dbbd8f5e10ba3231f6ebb5e3c94
Piangers <condeczar@gmail.com>
2018-04-17 14:49:07 -0300

    Nova estrategia: usando dicionário, ainda não esta 100%

--- download_ppp/extrai_dados_zip.py
+++ download_ppp/extrai_dados_zip.py
@@ -6,7 +6,7 @@
 from zipfile import ZipFile
 import os
 from re import search
-from collections import defaultdict
+
 
 
 def busca_zip(local_zip,local_destino):
@@ -14,35 +14,48 @@ def busca_zip(local_zip,local_destino):
     padrao = "^[\w\.-]+[\w\.-]+@([\w-]+\.)+[\w-]+[\w]+(RS|PR|SC|SP)-(HV|Base)-[1-9]+[0-9]+(.zip)+[\w]+(LIB)+[\w]+[1-9]+[0-9]+[1-9]+[0-9]+[1-9]+[0-9]+[1-9]+(.zip)*$"
     
     for root, dirs, files in os.walk(local_zip):
-        lista_zip = []
+        lista_zip = {}
+        
+        lista_pedaco_zip = []
         for f in files:    
             if search(padrao,f) and f.endswith('.zip') and os.path.isfile(os.path.join(root, f)):
                 
                 s1 = f.split('_')[1]
                 pedaco_nome_zip = s1.split('.')[0] # s2 recebe o trecho do nome do ZIP selecionado, que será comparado com o do endereço
-                lista_zip.append(pedaco_nome_zip) # procurar em todos os elementos nessa lista
-                lista_zip.sort()
+
+                caminho = os.path.join(root,f)
+
+                #lista_zip[pedaco_nome_zip] = caminho
+                lista_zip[f] = caminho
+
+                #lista_pedaco_zip.append(pedaco_nome_zip) # procurar em todos os elementos nessa lista
+                #lista_pedaco_zip.sort()
+
+
+
 
             endereco = []
             for root2, dirs, files in os.walk(local_destino):
 
+                todos_os_zips = lista_zip.keys()
+                
                 if root2.split(R'/')[-1] == "6_Processamento_PPP":
-                    caminho = root2
-                    pedaco_nome_endereco = caminho.split(R'/')[-2] 
-                    endereco.append(pedaco_nome_endereco)
-                    endereco.sort()
-            listas = [lista_zip,endereco]
-            print listas
-            keys = defaultdict(list)
-            for key, value in enumerate(listas):
+                    pai = root2.split(R'/')[-2]
 
-            # Adiciona o índice do valor na lista de índices:
-                keys[value].append(key)
-            # Exibe o resultado:
-            for value in keys:
-                if len(keys[value]) > 1:
-                    print(value, keys[value])
+                    for z in todos_os_zips:
+                        nome = z.split('_')[1].split('.')[0]
                         
+                        if pai == nome:
+                            caminho_zip = lista_zip[z]
+                            
+                            #EXTRAIR AQUI
+                   
+                            with ZipFile(caminho_zip,"r") as zip_ref:
+                                zip_ref.extract(root2)
+                        
+                    
+
+            
             
 
                         
