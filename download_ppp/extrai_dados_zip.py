eae2bf81e077a30286cb9b29f184b31b30e84c38
Piangers <condeczar@gmail.com>
2018-04-23 17:47:27 -0300

    prepara_ppp versão concluída 1.1

--- download_ppp/extrai_dados_zip.py
+++ download_ppp/extrai_dados_zip.py
@@ -1,13 +1,17 @@
 # -*- coding: utf-8 -*-
 
+##DSG=group
+##Local_do_zip=folder
+##Local_do_destino=folder
 
+import sys
 from zipfile import ZipFile
 import os
 from re import search
 import shutil
 
 
-def busca_zip(local_zip,local_destino):
+def extrai_zip(local_zip,local_destino):
 
     padrao = "^[\w\.-]+[\w\.-]+@([\w-]+\.)+[\w-]+[\w]+(RS|PR|SC|SP)-(HV|Base)-[1-9]+[0-9]+(.zip)+[\w]+(LIB)+[\w]+[1-9]+[0-9]+[1-9]+[0-9]+[1-9]+[0-9]+[1-9]+(.zip)*$"
     
@@ -19,15 +23,9 @@ def busca_zip(local_zip,local_destino):
             if search(padrao,f) and f.endswith('.zip') and os.path.isfile(os.path.join(root, f)):
                 
                 s1 = f.split('_')[1]
-                pedaco_nome_zip = s1.split('.')[0] # s2 recebe o trecho do nome do ZIP selecionado, que será comparado com o do endereço
-
+                pedaco_nome_zip = s1.split('.')[0] 
                 caminho = os.path.join(root,f)
-
-                #lista_zip[pedaco_nome_zip] = caminho
                 lista_zip[f] = caminho
-
-                #lista_pedaco_zip.append(pedaco_nome_zip) # procurar em todos os elementos nessa lista
-                #lista_pedaco_zip.sort()
 
     i = 0
     todos_os_zips = lista_zip.keys()
@@ -42,7 +40,6 @@ def busca_zip(local_zip,local_destino):
             if local.split(R'/')[-1] == "6_Processamento_PPP":
                 pai = local.split(R'/')[-2]
                 
-            #print z
             if pai == nome:
                 existe = True
 
@@ -52,19 +49,14 @@ def busca_zip(local_zip,local_destino):
                     print(frase)
                     continue
 
-                
-                with ZipFile(caminho_zip,"r") as zip_ref: # carrega o zip
-                            
+                with ZipFile(caminho_zip,"r") as zip_ref:     
                     zip_obj = ZipFile(caminho_zip)
-                        # percorre todos arquivos dentro do zip.
                     for ob in zip_obj.namelist():
                         filename = os.path.basename(ob)
-                        
-                        # pula diretórios
+    
                         if not filename:
                             continue
 
-                        # copia o arquivo para o diretório destino (em vez de extraí-lo)
                         abre = zip_ref.open(ob)
                         pega = file(os.path.join(local, filename), "wb")
                         with abre, pega:
@@ -75,12 +67,14 @@ def busca_zip(local_zip,local_destino):
                     print (frase)
 
                     
-        # Mostrar MI se a pasta ja tiver arquivos
         if not existe:
             frase = "O ZIP para o MI "+ nome + " não possui pasta correspondente!"
             frase.decode("utf8")
             print (frase)
             
-                
-#busca_zip(R'/home/piangers/Documentos/desenvolvimento/preparo/zip_ppp_exemplo', R'/home/piangers/Documentos/desenvolvimento/preparo/2018-04-04')
+#referencia para locais.               
+#extrai_zip(R'/home/piangers/Documentos/desenvolvimento/preparo/zip_ppp_exemplo', R'/home/piangers/Documentos/desenvolvimento/preparo/2018-04-04')
+
+if __name__== '__builtins__':
+    extrai_zip(local_do_zip,local_do_destino)
 
