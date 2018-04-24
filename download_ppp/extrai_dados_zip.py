b62d5a581364295ff516bd30de604c24070ab487
Piangers <condeczar@gmail.com>
2018-04-24 14:18:17 -0300

    script extrai_dados_zip comentado

--- download_ppp/extrai_dados_zip.py
+++ download_ppp/extrai_dados_zip.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
 ##DSG=group
-##Local_do_zip=folder
-##Local_do_destino=folder
+##local_do_zip=folder
+##local_do_destino=folder
 
 import sys
 from zipfile import ZipFile
@@ -16,63 +16,67 @@ def extrai_zip(local_zip,local_destino):
     padrao = "^[\w\.-]+[\w\.-]+@([\w-]+\.)+[\w-]+[\w]+(RS|PR|SC|SP)-(HV|Base)-[1-9]+[0-9]+(.zip)+[\w]+(LIB)+[\w]+[1-9]+[0-9]+[1-9]+[0-9]+[1-9]+[0-9]+[1-9]+(.zip)*$"
     
     for root, dirs, files in os.walk(local_zip):
-        lista_zip = {}
+        lista_zip = {} # inicia dicionario
         
-        lista_pedaco_zip = []
+        
         for f in files:    
             if search(padrao,f) and f.endswith('.zip') and os.path.isfile(os.path.join(root, f)):
                 
                 s1 = f.split('_')[1]
-                pedaco_nome_zip = s1.split('.')[0] 
-                caminho = os.path.join(root,f)
-                lista_zip[f] = caminho
-
+                pedaco_nome_zip = s1.split('.')[0] # s2 recebe o trecho do nome do ZIP selecionado, que será comparado com o do endereço
+                caminho = os.path.join(root,f) 
+                lista_zip[f] = caminho # cada zip encontrado em f será enviado para o dicionario como seu caminho
+        print lista_zip
     todos_os_zips = lista_zip.keys()
     for z in todos_os_zips:
         caminho_zip = lista_zip[z]
         nome = z.split('_')[1].split('.')[0]
                 
-        existe = False
+        existe = False # condição booleana
         
         for local, dirs, files in os.walk(local_destino):
-            pai = ''
+            pai = '' # inicia o variavel pai vazia
             if local.split(R'/')[-1] == "6_Processamento_PPP":
-                pai = local.split(R'/')[-2]
-                
+                pai = local.split(R'/')[-2] # pega pedaco do local pretendido
+            
             if pai == nome:
-                existe = True
-
+                existe = True # altera condição booleana
                 if len(os.listdir(local)) > 0:
                     frase = "O diretório " + local + " já contém arquivos!"
                     frase.decode("utf8")
                     print(frase)
                     continue
-
-                with ZipFile(caminho_zip,"r") as zip_ref:     
-                    zip_obj = ZipFile(caminho_zip)
-                    for ob in zip_obj.namelist():
-                        filename = os.path.basename(ob)
-    
-                        if not filename:
+                
+                with ZipFile(caminho_zip,"r") as zip_ref: # carrega o zip         
+                    zip_obj = ZipFile(caminho_zip) 
+                    
+                    for ob in zip_obj.namelist(): # percorre todos arquivos dentro do zip.
+                        filename = os.path.basename(ob) 
+                        
+                        if not filename: # pula diretórios
                             continue
-
+                        # copia os arquivos para o diretório destino (em vez de extraí-lo)
                         abre = zip_ref.open(ob)
                         pega = file(os.path.join(local, filename), "wb")
+                       
                         with abre, pega:
                             shutil.copyfileobj(abre, pega)
                     
-                    frase = "O ZIP para o MI "+ nome + " foi extraído com sucesso!"
-                    frase.decode("utf8")
-                    print (frase)
-              
+        # Mostrar MI se a pasta ja tiver arquivos
         if not existe:
             frase = "O ZIP para o MI "+ nome + " não possui pasta correspondente!"
             frase.decode("utf8")
             print (frase)
             
-#referencia para locais.               
+#referência para locais.               
 #extrai_zip(R'/home/piangers/Documentos/desenvolvimento/preparo/zip_ppp_exemplo', R'/home/piangers/Documentos/desenvolvimento/preparo/2018-04-04')
 
-if __name__== '__builtins__':
+if __name__== '__builtin__':
     extrai_zip(local_do_zip,local_do_destino)
 
+
+if __name__ == '__main__':
+    if len(sys.argv) == 3:
+        extrai_zip(sys.argv[1],sys.argv[2])
+    else:
+        print(u'Parametros incorretos!')
--- download_ppp/preparo_ppp.py
+++ download_ppp/preparo_ppp.py
@@ -16,23 +16,23 @@ def cria_pastas_e_zipa(caminho):
     pasta2 = '7_processamento_TBC_RBMC'
     padrao = "^(RS|PR|SC|SP)-(HV|Base)-[1-9]+[0-9]*$"
     nome_arq = "^(RS|PR|SC|SP)-(HV|Base)-[1-9]+[0-9]\.+[0-9]+[0-9]+(o|n)*$"
-    for root, dirs, files in os.walk(caminho):
+    
                               
             # Cria zipfile
-        for root, dirs, files in os.walk(caminho):
+    for root, dirs, files in os.walk(caminho):
         #Cria as pastas
-            if search(padrao,root.split(R"/")[-1]):
-                if dirs.count(pasta1) == 0:
-                    os.mkdir (os.path.join(root, pasta1))
-                if dirs.count(pasta2) == 0:
-                    os.mkdir (os.path.join(root, pasta2))                             
+        if search(padrao,root.split(R"/")[-1]):
+            if dirs.count(pasta1) == 0:
+                os.mkdir (os.path.join(root, pasta1))
+            if dirs.count(pasta2) == 0:
+                os.mkdir (os.path.join(root, pasta2))                             
                 # Cria zipfile
-            if root.split('/')[-1] == "2_RINEX" and len(files)>0 :
-                nome = root.split('/')[-2] + ".zip"
-                with ZipFile(os.path.join(root,nome), 'w') as novo_zip:
-                    for fl in files:
-                        if search(nome_arq,fl):
-                            novo_zip.write(os.path.join(root,fl),fl)
+        if root.split('/')[-1] == "2_RINEX" and len(files)>0 :
+            nome = root.split('/')[-2] + ".zip"
+            with ZipFile(os.path.join(root,nome), 'w') as novo_zip:
+                for fl in files:
+                    if search(nome_arq,fl):
+                        novo_zip.write(os.path.join(root,fl),fl)
 
 # referência de caminho.
 #cria_pastas_e_zipa(R'/home/piangers/Documentos/desenvolvimento/preparo/2018-04-04')            
